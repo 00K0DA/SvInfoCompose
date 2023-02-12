479e97fd5174f90cf15ec792986b647437ae7be5
00K0DA <yushi.ookoda.tuba@gmail.com>
2023-02-13 01:12:26 +0900

    特性用データクラス作成

---
+++ app/src/main/java/com/oukoda/svinfocompose/model/dataclass/Ability.kt
@@ -0,0 +1,23 @@
+package com.oukoda.svinfocompose.model.dataclass
+
+import org.json.JSONObject
+
+data class Ability(
+    val abilityId: Int,
+    val name: String,
+    val description: String
+) {
+    companion object {
+        private const val JSON_KEY_ABILITY_ID = "ability_id"
+        private const val JSON_KEY_NAME = "name"
+        private const val JSON_KEY_DESCRIPTION = "description"
+
+        fun fromJson(jsonObject: JSONObject): Ability {
+            return Ability(
+                abilityId = jsonObject.getInt(JSON_KEY_ABILITY_ID),
+                name = jsonObject.getString(JSON_KEY_NAME),
+                description = jsonObject.getString(JSON_KEY_DESCRIPTION)
+            )
+        }
+    }
+}
