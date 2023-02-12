ef9125da1648f011c37fd513bcc602301a96c1e0
00K0DA <yushi.ookoda.tuba@gmail.com>
2023-02-13 01:30:40 +0900

    Warning解消

--- app/src/main/java/com/oukoda/svinfocompose/model/dataclass/Ability.kt
+++ app/src/main/java/com/oukoda/svinfocompose/model/dataclass/Ability.kt
@@ -5,7 +5,7 @@ import org.json.JSONObject
 data class Ability(
     val abilityId: Int,
     val name: String,
-    val description: String
+    val description: String,
 ) {
     companion object {
         private const val JSON_KEY_ABILITY_ID = "ability_id"
@@ -16,7 +16,7 @@ data class Ability(
             return Ability(
                 abilityId = jsonObject.getInt(JSON_KEY_ABILITY_ID),
                 name = jsonObject.getString(JSON_KEY_NAME),
-                description = jsonObject.getString(JSON_KEY_DESCRIPTION)
+                description = jsonObject.getString(JSON_KEY_DESCRIPTION),
             )
         }
     }
--- app/src/test/java/com/oukoda/svinfocompose/model/dataclass/AbilityTest.kt
+++ app/src/test/java/com/oukoda/svinfocompose/model/dataclass/AbilityTest.kt
@@ -7,7 +7,7 @@ import org.junit.Test
 class AbilityTest {
 
     @Test
-    fun fromJsonTest(){
+    fun fromJsonTest() {
         val ability = Ability.fromJson(JSONObject(testJson1))
         assertEquals(211, ability.abilityId)
         assertEquals("ARシステム", ability.name)
@@ -19,6 +19,5 @@ class AbilityTest {
         "name": "AR\u30b7\u30b9\u30c6\u30e0",
         "description": "\u6301\u3063\u3066\u3044\u308b\u5c02\u7528\u306e\u9053\u5177\u300e\u30e1\u30e2\u30ea\u300f\u306e\u7a2e\u985e\u306b\u5bfe\u5fdc\u3057\u3066\u81ea\u5206\u306e\u300e\u30bf\u30a4\u30d7\u300f\u304c\u5909\u5316\u3059\u308b\u3002"
     }"""
-
     }
 }
