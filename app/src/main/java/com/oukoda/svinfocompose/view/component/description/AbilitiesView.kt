23b9cea74c92d01a843f899d8413feb00a472ce9
00K0DA <yushi.ookoda.tuba@gmail.com>
2023-03-02 00:47:16 +0900

    Add AbilityView

---
+++ app/src/main/java/com/oukoda/svinfocompose/view/component/description/AbilitiesView.kt
@@ -0,0 +1,66 @@
+package com.oukoda.svinfocompose.view.component.description
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.oukoda.svinfocompose.R
+import com.oukoda.svinfocompose.model.dataclass.Ability
+
+@Composable
+fun AbilitiesView(
+    ability1st: Ability,
+    ability2nd: Ability,
+    hiddenAbility: Ability,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        AbilityView(abilityLabelId = R.string.description_ability_1st, ability = ability1st)
+        if (ability1st.abilityId != ability2nd.abilityId) {
+            AbilityView(abilityLabelId = R.string.description_ability_2nd, ability = ability2nd)
+        }
+        AbilityView(abilityLabelId = R.string.description_ability_hidden, ability = hiddenAbility)
+    }
+}
+
+@Composable
+private fun AbilityView(abilityLabelId: Int, ability: Ability, modifier: Modifier = Modifier) {
+    var isExpand by remember { mutableStateOf(false) }
+    Column(
+        modifier = modifier.clickable { isExpand = !isExpand },
+        horizontalAlignment = Alignment.Start,
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                modifier = Modifier.width(60.dp),
+                text = stringResource(id = abilityLabelId),
+                fontSize = 14.sp,
+            )
+            Text(text = ability.name, fontSize = 20.sp)
+        }
+        AnimatedVisibility(visible = isExpand) {
+            Text(text = ability.description, fontSize = 12.sp)
+        }
+    }
+}
--- app/src/main/java/com/oukoda/svinfocompose/view/page/ListPage.kt
+++ app/src/main/java/com/oukoda/svinfocompose/view/page/ListPage.kt
@@ -1,30 +1,56 @@
 package com.oukoda.svinfocompose.view.page
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.oukoda.svinfocompose.model.dataclass.Pokemon
 import com.oukoda.svinfocompose.model.enumclass.BottomItems
 import com.oukoda.svinfocompose.model.rememberForeverLazyListState
 import com.oukoda.svinfocompose.repository.JsonRepository
 import com.oukoda.svinfocompose.view.component.ListItemView
+import com.oukoda.svinfocompose.view.component.listpage.DescriptionView
 
 @Composable
 fun ListPage() {
+    val ROUTE_LIST = "list"
+    val ROUTE_DESCRIPTION = "desc"
     val repository = JsonRepository(resource = LocalContext.current.resources)
     val pokemonList = repository.getPokemonList()
-    LazyColumn(
-        state = rememberForeverLazyListState(key = BottomItems.List.route()),
-        modifier = Modifier.padding(horizontal = 16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-    ) {
-        itemsIndexed(pokemonList) { index, pokemon ->
-            ListItemView(number = index, pokemon = pokemon, onTap = {})
+    val navController = rememberNavController()
+    var selectedPokemon: Pokemon? = null
+
+    NavHost(navController = navController, startDestination = ROUTE_LIST) {
+        composable(ROUTE_LIST) {
+            LazyColumn(
+                state = rememberForeverLazyListState(key = BottomItems.List.route()),
+                modifier = Modifier.padding(horizontal = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                item { Spacer(modifier = Modifier.height(16.dp)) }
+                itemsIndexed(pokemonList) { index, pokemon ->
+                    ListItemView(number = index, pokemon = pokemon, onTap = {
+                        selectedPokemon = pokemon
+                        navController.navigate(ROUTE_DESCRIPTION) {
+                            launchSingleTop = true
+                        }
+                    })
+                }
+                item { Spacer(modifier = Modifier.height(16.dp)) }
+            }
+        }
+
+        composable(ROUTE_DESCRIPTION) {
+            DescriptionView(selectedPokemon!!, repository) { navController.navigate(ROUTE_LIST) }
         }
     }
 }
