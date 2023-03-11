9113710b6bf4f310996ebf8748ba536455db3bb8
00K0DA <60129504+00K0DA@users.noreply.github.com>
2023-03-11 23:31:18 +0900

    ひらがなをカタカナに変換するクラスを作成

---
+++ app/src/test/java/com/oukoda/svinfocompose/model/dataclass/Hira2KataConverterTest.kt
@@ -0,0 +1,109 @@
+package com.oukoda.svinfocompose.model.dataclass
+
+import com.oukoda.svinfocompose.util.Hira2KanaConverter
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+@Suppress("NonAsciiCharacters", "TestFunctionName")
+class Hira2KataConverterTest {
+    @Test
+    fun test_ひらがな一文字が変換できること() {
+        val testTaskList: List<Pair<String, String>> = listOf(
+            Pair("あ", "ア"),
+            Pair("い", "イ"),
+            Pair("う", "ウ"),
+            Pair("え", "エ"),
+            Pair("お", "オ"),
+            Pair("か", "カ"),
+            Pair("き", "キ"),
+            Pair("く", "ク"),
+            Pair("け", "ケ"),
+            Pair("こ", "コ"),
+            Pair("さ", "サ"),
+            Pair("し", "シ"),
+            Pair("す", "ス"),
+            Pair("せ", "セ"),
+            Pair("そ", "ソ"),
+        )
+        testTaskList.forEach { testTask ->
+            convertTest(testTask.first, testTask.second)
+        }
+    }
+
+    @Test
+    fun test_小文字の一文字が変換できること() {
+        val testTaskList: List<Pair<String, String>> = listOf(
+            Pair("ぁ", "ァ"),
+            Pair("ぃ", "ィ"),
+            Pair("ぅ", "ゥ"),
+            Pair("ぇ", "ェ"),
+            Pair("ぉ", "ォ"),
+            Pair("っ", "ッ"),
+            Pair("ゃ", "ャ"),
+            Pair("ゅ", "ュ"),
+            Pair("ょ", "ョ"),
+        )
+        testTaskList.forEach { testTask ->
+            convertTest(testTask.first, testTask.second)
+        }
+    }
+
+    @Test
+    fun test_複数文字のひらがなが変換されること() {
+        val testTaskList: List<Pair<String, String>> = listOf(
+            Pair("うそっきー", "ウソッキー"),
+            Pair("げんがー", "ゲンガー"),
+            Pair("がぶりあす", "ガブリアス"),
+            Pair("ぎゃらどす", "ギャラドス"),
+        )
+        testTaskList.forEach { testTask ->
+            convertTest(testTask.first, testTask.second)
+        }
+    }
+
+    @Test
+    fun test_カタカナが変換されないこと() {
+        val testTaskList: List<Pair<String, String>> = listOf(
+            Pair("ア", "ア"),
+            Pair("イ", "イ"),
+            Pair("ウ", "ウ"),
+            Pair("エ", "エ"),
+            Pair("オ", "オ"),
+            Pair("ァ", "ァ"),
+        )
+        testTaskList.forEach { testTask ->
+            convertTest(testTask.first, testTask.second)
+        }
+    }
+
+    @Test
+    fun test_ひらがなのみが変換されること() {
+        val testTaskList: List<Pair<String, String>> = listOf(
+            Pair("ガブリあす", "ガブリアス"),
+            Pair("ふしギダネ", "フシギダネ"),
+            Pair("りザーどン", "リザードン"),
+            Pair("ミずゴろウ", "ミズゴロウ"),
+            Pair("半分、青い", "半分、青イ"),
+        )
+        testTaskList.forEach { testTask ->
+            convertTest(testTask.first, testTask.second)
+        }
+    }
+
+    @Test
+    fun test_漢字が変換されないこと() {
+        val testTaskList: List<Pair<String, String>> = listOf(
+            Pair("勇気凛凛", "勇気凛凛"),
+            Pair("元気溌剌", "元気溌剌"),
+            Pair("興味津々", "興味津々"),
+            Pair("意気揚々", "意気揚々"),
+        )
+        testTaskList.forEach { testTask ->
+            convertTest(testTask.first, testTask.second)
+        }
+    }
+
+    private fun convertTest(input: String, expected: String) {
+        assertEquals(expected, Hira2KanaConverter.convert(input))
+    }
+}
