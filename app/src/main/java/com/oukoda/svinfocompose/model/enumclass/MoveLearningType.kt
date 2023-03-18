a18986c106169e07607ab251c14ba931d6570fb5
00K0DA <60129504+00K0DA@users.noreply.github.com>
2023-03-18 23:03:00 +0900

    現状

--- app/src/main/java/com/oukoda/svinfocompose/model/enumclass/MoveLearningType.kt
+++ app/src/main/java/com/oukoda/svinfocompose/model/enumclass/MoveLearningType.kt
@@ -2,7 +2,7 @@ package com.oukoda.svinfocompose.model.enumclass
 
 import com.oukoda.svinfocompose.R
 
-enum class MoveLearningType(stringId: Int) {
+enum class MoveLearningType(val stringId: Int) {
     Level(R.string.move_learning_type_level),
     Machine(R.string.move_learning_type_machine),
     Egg(R.string.move_learning_type_egg),
